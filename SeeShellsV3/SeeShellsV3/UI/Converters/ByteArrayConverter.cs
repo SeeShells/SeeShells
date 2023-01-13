2db767b326d3a94764880eb05f93cc644ba50313
jakenorris1124 <jakenorris1124@gmail.com>
2023-01-13 15:25:42 -0500

    Fixed Value incorrectly displaying the data type

---
+++ SeeShellsV3/SeeShellsV3/UI/Converters/ByteArrayConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SeeShellsV3.UI.Converters
+{
+    public class ByteArrayConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte[] bytes)
+            {
+                return bytes.Aggregate("", (current1, current) => current1 + current.ToString() + ",");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
