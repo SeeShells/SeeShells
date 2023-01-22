09ed508060a16878de3c1bf80519e54bfc1b76ef
jakenorris1124 <jakenorris1124@gmail.com>
2023-01-22 15:21:37 -0500

    Wrapper class for multiple converters in a binding

---
+++ SeeShellsV3/SeeShellsV3/UI/Converters/Wrappers/FieldConverterWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using SeeShellsV3.UI.Converters;
+
+namespace SeeShellsV3.UI.Converters
+{
+    /// <summary>
+    /// Wrapper class that allows for the use of multiple converters in a single binding
+    /// </summary>
+    public class FieldConverterWrapper: IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private IValueConverter[] Converters = new IValueConverter[]
+        {
+            new ByteArrayConverter(), 
+            new FileSizeConverter()
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (IValueConverter converter in Converters)
+            {
+                value = converter.Convert(value, targetType, parameter, culture);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (IValueConverter converter in Converters)
+            {
+                value = converter.ConvertBack(value, targetType, parameter, culture);
+            }
+
+            return value;
+        }
+    }
+}
