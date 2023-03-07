42b33deba4563dccf710d7ae463c797d1a1278b0
jakenorris1124 <jakenorris1124@gmail.com>
2023-03-06 22:07:42 -0500

    Groundwork for adding events to reports

---
+++ SeeShellsV3/SeeShellsV3/Repositories/ReportEventCollection/IReportEventCollection.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using SeeShellsV3.Data;
+
+namespace SeeShellsV3.Repositories
+{
+    /// <summary>
+    /// A collection that holds ShellEvents to be used in specific PDF modules.
+    /// </summary>
+    public interface IReportEventCollection
+    {
+        /// <summary>
+        /// Collection that stores the selected ShellEvents.
+        /// </summary>
+        IShellEventCollection SelectedEvents { get; set; }
+
+        /// <summary>
+        /// Boolean that signifies whether the collection is empty or not.
+        /// </summary>
+        bool HasEvents { get; set; }
+
+        /// <summary>
+        /// Adds an event to the collection.
+        /// </summary>
+        /// <param name="shellEvent">Event to be added to the collection.</param>
+        /// <returns>Boolean to signify whether the operation was successful.</returns>
+        bool AddEvent(IShellEvent shellEvent);
+
+        /// <summary>
+        /// Removes an event to the collection.
+        /// </summary>
+        /// <param name="shellEvent">Event to be removed from the collection.</param>
+        /// <returns>Boolean to signify whether the operation was successful.</returns>
+        bool RemoveEvent(IShellEvent shellEvent);
+    }
+}
---
+++ SeeShellsV3/SeeShellsV3/Repositories/ReportEventCollection/ReportEventCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using SeeShellsV3.Data;
+
+namespace SeeShellsV3.Repositories
+{
+    public class ReportEventCollection: IReportEventCollection
+    {
+        public IShellEventCollection SelectedEvents { get; set; }
+        public bool HasEvents { get; set; }
+        public bool AddEvent(IShellEvent shellEvent)
+        {
+            try
+            {
+                SelectedEvents.Add(shellEvent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool RemoveEvent(IShellEvent shellEvent)
+        {
+            try
+            {
+                return SelectedEvents.Remove(shellEvent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
